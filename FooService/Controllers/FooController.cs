fdde2ff4713744fae4ae854e7ea1972568e828b7
Sam Pratt <Sam.Pratt@bighand.com>
2021-03-02 09:24:47 +0000

    Added Foo mock service

---
+++ FooService/Controllers/FooController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FooService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<String> Get()
+        {
+            return "Foo";
+        }
+    }
+}
